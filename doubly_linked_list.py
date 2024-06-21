737ed67036c91ebfbfad4d2978c47536897b41d9
Muhammad Hafiz Azhari <mhafiz.azh@gmail.com>
2024-06-21 00:08:44 -0600

    Update doubly_linked_list.py

--- doubly_linked_list.py
+++ doubly_linked_list.py
@@ -18,5 +18,28 @@ class DoublyLinkedList:
             print(temp.value)
             temp = temp.next
 
-    def append(self):
-        pass
+    def append(self, value):
+        new_node = Node(value)
+        if self.head is None:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            self.tail.next = new_node
+            new_node.prev = self.tail
+            self.tail = new_node
+        self.length += 1
+        return True
+
+    def pop(self):
+        temp = self.tail
+        if self.head is None:
+            return False
+        if self.head == self.tail:
+            self.head = self.tail = None
+            self.length -= 1
+        else:
+            self.tail = self.tail.prev
+            self.tail.next = None
+            temp.prev = None
+            self.length -= 1
+            return temp
