250b68cd67c8183dc698b69f35fb23474c475af0
Muhammad Hafiz Azhari <mhafiz.azh@gmail.com>
2024-06-24 01:02:37 -0600

    UPDATE LINKEDLIST

--- linkedlist/linked_list.py
+++ linkedlist/linked_list.py
@@ -20,6 +20,51 @@ class LinkedList:
             self.tail = new_node
         self.length += 1
 
+    def prepend(self, value):
+        new_node = Node(value)
+        if self.length == 0:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            new_node.next = self.head
+            self.head = new_node
+        self.length += 1
+
+    def insert(self, index, value):
+        if index < 0 or index > self.length:
+            return None
+        if index == 0:
+            return self.prepend(value)
+        if index == self.length:
+            return self.append(value)
+        new_node = Node(value)
+        temp = self.get(index - 1)
+        new_node.next = temp.next
+        temp.next = new_node
+        self.length += 1
+
+    def remove(self, index):
+        if index < 0 or index >= self.length:
+            return None
+        if index == 0:
+            return self.pop_first()
+        temp = self.get(index - 1)
+        removed_node = temp.next
+        temp.next = removed_node.next
+        removed_node.next = None
+        self.length -= 1
+        if self.length == 0:
+            self.tail = None
+        return removed_node
+
+    def get(self, index):
+        if index < 0 or index >= self.length:
+            return None
+        temp = self.head
+        for _ in range(index):
+            temp = temp.next
+        return temp
+
     def pop_first(self):
         if self.length == 0:
             return None
@@ -30,3 +75,9 @@ class LinkedList:
         if self.length == 0:
             self.tail = None
         return temp
+
+    def print_list(self):
+        current_node = self.head
+        while current_node is not None:
+            print(current_node.value)
+            current_node = current_node.next
